34f557c5707719398efc9de8df774cb7d1414913
KavenZohar <kaven0504@gmail.com>
2025-02-01 20:00:33 +0700

    create authApiRoutes.js

---
+++ src/routes/api/authApiRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+const authApiRoutes = express.Router();
+
+// importing middleware functions for admin authentication
+import { authAdminLogin, authAdminLogout } from "../../middlewares/auth/adminAuth.js";
+
+// router for admin login, using authAdminLogin middleware
+authApiRoutes.post("/admin/login", authAdminLogin);
+
+// router for admin logout, using authAdminLogout middleware
+authApiRoutes.post("/admin/logout", authAdminLogout);
+
+export default authApiRoutes; // export to lib/app.js
