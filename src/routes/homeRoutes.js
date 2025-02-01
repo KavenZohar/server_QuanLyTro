629155d135b7581d599b5174af1ce78ac7fbd588
KavenZohar <kaven0504@gmail.com>
2025-02-01 19:41:22 +0700

    update homeRoutes.js

---
+++ src/routes/homeRoutes.js
@@ -0,0 +1,17 @@
+// route to handle tasks when the admin or client is not logged in
+
+import express from "express";
+const homeRoutes = express.Router();
+
+import loginCheck from "../middlewares/validation/checkLogin.js";
+
+import getHomeController from "../controllers/userControllers/getHomeController.js";
+import getLoginController from "../controllers/userControllers/adminControllers/getLoginController.js";
+
+homeRoutes.get("/", getHomeController);
+
+// admin login route: The checkLog function checks whether the admin is logged in.
+// if already logged in, redirect to the admin home page, otherwise send the admin login route.
+homeRoutes.get("/auth/admin/login", [loginCheck, getLoginController]);
+
+export default homeRoutes;
