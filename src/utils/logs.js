09d6f918f5688d4e4b0e5230880864c101632627
KavenZohar <kaven0504@gmail.com>
2025-02-01 21:41:28 +0700

    update logs.js add loginHistory func

--- src/utils/logs.js
+++ src/utils/logs.js
@@ -3,14 +3,16 @@ import fs from "fs";
 function Log(file, message) {
     const date = `==>[${new Date().toISOString()}]`;
 
+    console.log(date + message);
+
     if (fs.existsSync(file)) { // check if the file already exists
         fs.appendFile(file, date + message, (err) => { // add content
-            if (err) {
-                console.error("Error while writing log:", err);
+            if (err) {("Error while writing log:", err);
             }
+                console.error
         });
     } else { // if the file does not exist, create it and add content
-        fs.writeFile(file, message, (err) => {
+        fs.writeFile(file, date + message, (err) => {
             if (err) {
                 console.error("Error while writing log:", err);
             }
@@ -23,7 +25,7 @@ class log {
     // save server error
     static ErrorServer(content) {
         const file = "./logs/server_errors.log";
-        const message = `--ERROR: "${content}"\n`;
+        const message = `--ERROR: "${content}".\n`;
 
         Log(file, message);
     }
@@ -32,7 +34,7 @@ class log {
     // save client error
     static ErrorClient(content) {
         const file = "./logs/client_errors.log";
-        const message = `--ERROR by Client: "${content}"\n`;
+        const message = `--ERROR by Client: "${content}".\n`;
 
         Log(file, message);
     }
@@ -41,7 +43,7 @@ class log {
     // save strange request
     static StrangeRequest(req) {
         const file = "./logs/strange_request.log";
-        const message = `--[IP: ${req.ip}]: "${req.method}", "${req.hostname}${req.originalUrl}", "IPs: ${req.ips}"\n`;
+        const message = `--[IP: ${req.ip}]: "${req.method}", "${req.hostname}${req.originalUrl}", "IPs: ${req.ips}".\n`;
 
         Log(file, message);
     }
@@ -50,7 +52,16 @@ class log {
     // save database error
     static dbErrors(content) {
         const file = "./logs/db_errors.log";
-        const message = `--DB ERROR: "${content}"\n`;
+        const message = `--DB ERROR: "${content}".\n`;
+
+        Log(file, message);
+    }
+
+
+    // save login history
+    static loginHistory(req, user, role) {
+        const file = "./logs/login_history.log";
+        const message = `--LOGIN USER--[IP: ${req.ip}]: username:"${user.username}" role:"${role}".\n`;
 
         Log(file, message);
     }
