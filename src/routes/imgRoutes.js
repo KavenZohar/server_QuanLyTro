d559b4b97a7e9e2dd8af0650b5324074362dd29d
KavenZohar <kaven0504@gmail.com>
2025-02-02 23:15:27 +0700

    update

--- src/routes/imgRoutes.js
+++ src/routes/imgRoutes.js
@@ -6,7 +6,7 @@ const imgRoutes = express.Router();
 // import the imgAuth function to handle key authentication
 import imgAuth from "../middlewares/auth/imgAuth.js";
 
-import { sendRoomImgController, sendAvatarImgController } from "../controllers/userControllers/sendImgController.js";
+import { sendRoomImgController, sendAvatarImgController } from "../controllers/otherControllers/sendImgController.js";
 
 imgRoutes.get("/room/:imgName", [imgAuth.checkKey(), sendRoomImgController]); // check the key, if key is correct, sendRoomImgController will send an image
 imgRoutes.get("/avatar/:imgName", [imgAuth.checkKey(), sendAvatarImgController]); // check the key, if key is correct, sendAvatarImgController will send an image
